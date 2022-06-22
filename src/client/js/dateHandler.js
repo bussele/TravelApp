68b5f954fcd16a2b2aafdae8373dcff8b76062e5
Behrens Verena <q336796@europe.bmw.corp>
2022-06-22 09:15:58 +0200

    17th Commit

--- src/client/js/dateHandler.js
+++ src/client/js/dateHandler.js
@@ -1,6 +1,6 @@
 // Calculates difference between current & departing dates
 function dateDiff() {
-    let dateStrVal = document.getElementById('start').value
+    let dateStrVal = document.getElementById('trip-start').value
     let todayDate = new Date()
     let str = dateStrVal.split('-')
     let travelDate = new Date(str[0], str[1] - 1, str[2])
